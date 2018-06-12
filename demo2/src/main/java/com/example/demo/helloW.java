c833a2918a3b99f2a20719f08866cced3634f409
LENOVO <LENOVO@User>
2018-06-11 21:32:58 -0500

    segundo comment

--- demo2/src/main/java/com/example/demo/helloW.java
+++ demo2/src/main/java/com/example/demo/helloW.java
@@ -5,6 +5,7 @@ public class helloW {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("se agrega");
+		System.out.println("se agrega2");
 
 	}
 
